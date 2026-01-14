659ad220341284ce88a3ad49689d8c329685a47b
Harsh Rathour <harshrth1774@gmail.com>
2026-01-14 23:50:00 +0530

    DSA codes

--- bst/searching2.cpp
+++ bst/searching2.cpp
@@ -1,62 +1,25 @@
-#include<iostream>
-using namespace std;
-class Node{
-  public:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root == NULL) return NULL;
 
-  int data;
-  Node* right;
-  Node* left;
+        if(root->val == val) return root;
 
-  Node(int val)
-  {
-    data = val;
-    right = NULL;
-    left = NULL;
-  }
+        else if(root->val > val) return searchBST(root->left,val);
+        else return searchBST(root->right,val);
+
+        return root;
+    }
 };
-
-Node* insert(Node* root, int val)
-{
-  if(root == NULL){
-    Node* temp = new Node(val);
-    return temp;
-  }
-  
-
-  if(val < root->data){
-    root->left = insert(root->left,val);
-  }else{
-    root->right = insert(root->right,val);
-  }
-
-  return root;
-}
-
-bool search(Node* root, int target)
-{
-  if(root == NULL) return 0;
-
-  if(target == root->data) return 1;
-
-  if(target < root->data){
-    search(root->left,target);
-  }else{
-    search(root->right, target);
-  }
-}
-int main()
-{
-  int arr[] = {50,28,5,34,13};
-  
-  Node* root = NULL;
-
-  for(int i = 0; i<5; i++){
-   root =  insert(root,arr[i]);
-  }
-
-  int target;
-  cout<<"enter the target: ";
-  cin>>target;
-
-  cout<<search(root, target);
-}
