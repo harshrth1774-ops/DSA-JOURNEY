7ef58938658826bed2d04c69ec2ddd9ded2a05bd
Harsh Rathour <harshrth1774@gmail.com>
2026-02-20 17:38:33 +0530

    Added DSA practice solutions

--- binarytree/bt_implementation.cpp
+++ binarytree/bt_implementation.cpp
@@ -1,51 +1,42 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+
 class Node{
   public:
-
   int data;
-  Node* left;
-  Node* right;
+  Node* left,*right;
 
   Node(int val){
     data = val;
-    left = NULL;
-    right = NULL;
+    left = right = NULL;
   }
 };
 int main()
 {
-  int first,second;
-
   int x; cin>>x;
+  int first, second;
 
   queue<Node*>q;
 
   Node* root = new Node(x);
-  //put the address of the first or root node in the queue
   q.push(root);
 
-  //start building the binary tree
- 
-  while(!q.empty()){
-
+  while(!q.empty())
+  {
     Node* temp = q.front();
     q.pop();
 
-    //left node
-    cin>>first;
+    cin>>left;
     if(first != -1){
       temp->left = new Node(first);
       q.push(temp->left);
     }
 
-    //right node
-    cin>>second;
+    cin>>right;
     if(second != -1){
       temp->right = new Node(second);
       q.push(temp->right);
     }
-
   }
 }
--- binarytree/implemenation_using_rec.cpp
+++ binarytree/implemenation_using_rec.cpp
@@ -1,47 +1,27 @@
 #include<iostream>
 using namespace std;
-
-//creating node class
-
-class Node
-{
+class Node{
   public:
   int data;
-  Node* left;
-  Node* right;
+  Node* left, *right;
+  int data;
 
-  Node(int val)
-  {
+  Node(int val){
     data = val;
-    left = NULL;
-    right = NULL;
+    left  = right = NULL;
   }
 };
 
-//implementing binary tree using recursion
-  Node* binaryTree()
-  {
-    int x;
-    cin>>x;
-
-    if(x == -1) return NULL;
-    Node* temp = new Node(x);
-
-    //left side
-    cout<<"ENTER LEFT CHILD "<<x<<" ";
-    temp->left = binaryTree();
-    //right side
-     cout<<"ENTER RIGHT CHILD "<<x<<" ";
-    temp->right = binaryTree();
-
-    return temp;
-  }
-
-int main()
+Node* binaryTree()
 {
-  cout<<"enter the root node: ";
-  Node* root;
-  root = binaryTree();
-  binaryTree();
+  int x; cin>>x;
 
+  if(x == -1) return  NULL;
+
+  Node* temp = new Node(x);
+  
+  temp->left = binaryTree();
+  temp->right = binaryTree();
+
+  return temp;
 }
