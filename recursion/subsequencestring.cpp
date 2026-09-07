75a70c145632c43cddadf5f774fc54ed2f213789
Harsh Rathour <harshrth1774@gmail.com>
2026-09-07 23:53:24 +0530

    Update codes

--- recursion/subsequencestring.cpp
+++ recursion/subsequencestring.cpp
@@ -14,13 +14,13 @@ void subsequence(vector<string>& ans,string s, int idx, int n, string temp){
   temp.push_back(s[idx]);
   subsequence(ans,s,idx+1,n,temp);
 }
-int main(){
+int main(){ 
   
   string s = "abc";
   int n = s.size();
   string temp;
   vector<string>ans;
-
+ 
   subsequence(ans,s,0,n,temp);
     
   for(int i = 0; i<ans.size(); i++){
